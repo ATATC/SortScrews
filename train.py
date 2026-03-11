47b08786afb32f9aca79ea957e428f236e06a600
ATATC <futerry@outlook.com>
2026-03-10 20:29:29 -0400

    Routine updates.

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from sort_screws import SortScrewsDataset, EfficientNetTrainer
 
 if __name__ == "__main__":
     if not exists("SortScrews") and input("Dataset not found, download? (y/n) >>>") == "y":
-        download_dataset("atatc/esc102/SortScrews", "SortScrews")
+        download_dataset("atatc/ut/esc102/SortScrews", "SortScrews")
     dataset = SortScrewsDataset("SortScrews")
     train, val = dataset.fold(fold="all")
     train_dl = DataLoader(train, 16, True, num_workers=4, prefetch_factor=2, persistent_workers=True, pin_memory=True)
