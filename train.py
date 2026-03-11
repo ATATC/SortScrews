d5b8fffa7ba75d201be291f51a8505578859c393
ATATC <futerry@outlook.com>
2026-03-10 20:37:54 -0400

    Routine updates.

--- train.py
+++ train.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     val_dl = DataLoader(val, 1, False)
     trainer = EfficientNetTrainer("trainer", train_dl, val_dl, recoverable=False)
     trainer.num_classes = dataset.num_classes
-    trainer.train(100, seed=42)
+    trainer.train(100, seed=42, compile_model=False)
