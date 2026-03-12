eb9f637249ae20c1ba6cc6e2cfe58e057404cd08
ATATC <futerry@outlook.com>
2026-03-12 17:48:27 -0400

    Routine updates.

--- infer.py
+++ infer.py
@@ -4,6 +4,7 @@ from typing import override
 import cv2
 import numpy as np
 import torch
+from torchvision.transforms import Resize
 from mipcandy import HasDevice, Device, convert_logits_to_ids, auto_device
 
 from sort_screws import Camera, EfficientNetPredictor
@@ -16,6 +17,7 @@ class Predictor(Camera, HasDevice):
         self.predictor: EfficientNetPredictor = EfficientNetPredictor(str(experiment_folder), (3, 512, 512),
                                                                       device=device)
         self.paused: bool = False
+        self.resize: Resize = Resize(224)
 
     @override
     def job(self, frame: np.ndarray, roi: np.ndarray, bbox: tuple[int, int, int, int]) -> bool:
@@ -24,6 +26,7 @@ class Predictor(Camera, HasDevice):
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             image = torch.as_tensor(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB), dtype=torch.float,
                                      device=self._device).permute(2, 0, 1)
+            image = self.resize(image)
             logits = self.predictor.predict_image(image)
             class_id = convert_logits_to_ids(logits, channel_dim=0).item()
             cv2.putText(frame, f"Class: {class_id}", (20, 40), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2,
--- train.py
+++ train.py
@@ -2,15 +2,17 @@ from os import rename
 from os.path import exists
 
 from torch.utils.data import DataLoader
-from mipcandy import download_dataset, auto_device
+from torchvision.transforms import Resize
+from mipcandy import download_dataset, auto_device, MONAITransform, JointTransform
 
 from sort_screws import SortScrewsDataset, EfficientNetTrainer
 
 if __name__ == "__main__":
     if not exists("SortScrews") and input("Dataset not found, download? (y/n) >>>") == "y":
         download_dataset("atatc/ut/esc102/SortScrews", "SortScrews")
-    train = SortScrewsDataset("SortScrews", True)
-    val = SortScrewsDataset("SortScrews/validation", True)
+    resize = JointTransform(image_only=MONAITransform(Resize(224)))
+    train = SortScrewsDataset("SortScrews", True, transform=resize)
+    val = SortScrewsDataset("SortScrews/validation", True, transform=resize)
     if train.num_classes != val.num_classes:
         raise ValueError(f"Number of classes mismatch: train={train.num_classes}, val={val.num_classes}")
     train_dl = DataLoader(train, 16, True, num_workers=4, prefetch_factor=2, persistent_workers=True, pin_memory=True)
